dbd1d0b6d57523a3c742cb0b61719f96fab386af
JulianManrique <107586685+JulianManrique@users.noreply.github.com>
2026-05-09 23:06:22 +0000

    Initial structure frontend

---
+++ app/components/Hero.tsx
@@ -0,0 +1,8 @@
+    export default function Hero(){
+        return (
+            <section>
+                <h1>CREMOS MOMENTOS</h1>
+                <p>Pasteleria artesanal hecha con amor</p>
+            </section>
+        );
+    } 
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,14 @@ export default function Navbar(){
     return (
         <nav>
             <h2>Eotia</h2>
+
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Personalizados</li>
+                <li>Nosotros</li>
+                <li>Contactos</li>
+            </ul>
         </nav>
     );
 }
