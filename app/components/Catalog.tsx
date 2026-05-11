d5e9be35364d372bcf96e607603e21da3b0f5c45
JulianManrique <107586685+JulianManrique@users.noreply.github.com>
2026-05-11 18:40:44 +0000

    Improve catalog visual design

--- app/components/Catalog.tsx
+++ app/components/Catalog.tsx
@@ -1,14 +1,41 @@
-export default function Catalog(){
-    return (
-        <section>
-            <h2>Nuestros Productos</h2>
-            <div>
-                <article>Tartas</article>
-                <article>Alfajores</article>
-                <article>Galletas</article>
-                <article>Cheesecake</article>
-                <article>Brwonie</article>
-            </div>
-        </section>
-    );
+export default function Catalog() {
+  return (
+    <section className="catalog">
+      <h2>Nuestros Productos</h2>
+
+      <div className="catalog-container">
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1571115764595-644a1f56a55c"
+            alt="Tartas"
+          />
+          <h3>Tartas</h3>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1559620192-032c4bc4674e"
+            alt="Alfajores"
+          />
+          <h3>Alfajores</h3>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1499636136210-6f4ee915583e"
+            alt="Galletas"
+          />
+          <h3>Galletas</h3>
+        </article>
+
+        <article>
+          <img
+            src="https://images.unsplash.com/photo-1565958011703-44f9829ba187"
+            alt="Cheesecake"
+          />
+          <h3>Cheesecake</h3>
+        </article>
+      </div>
+    </section>
+  );
 }
