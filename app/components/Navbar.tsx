9f3497cd7928077bf92a919a8ada63db28ff83f9
Juli <wordjulian@hotmail.com>
2026-05-13 15:23:55 -0500

    segunda modificacion frontend

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,30 @@
-export default function Navbar(){
-    return (
-        <nav>
-            <h2>Eotia</h2>
+import Image from "next/image";
+import { FiSearch, FiUser, FiShoppingCart } from "react-icons/fi";
 
-            <ul>
-                <li>Inicio</li>
-                <li>Productos</li>
-                <li>Personalizados</li>
-                <li>Nosotros</li>
-                <li>Contactos</li>
-            </ul>
-        </nav>
-    );
+export default function Navbar() {
+  return (
+    <nav>
+  <Image
+    src="/logo.jpeg"
+    alt="Eotia Logo"
+    width={90}
+    height={90}
+    className="logo"
+  />
+
+  <ul>
+    <li>Inicio</li>
+    <li>Productos</li>
+    <li>Personalizados</li>
+    <li>Nosotros</li>
+    <li>Contacto</li>
+  </ul>
+
+  <div className="nav-icons">
+    <FiSearch />
+    <FiUser />
+    <FiShoppingCart />
+  </div>
+</nav>
+  );
 }
